81da39ea30632eb502584a5092ff0ee82f0cbf7c
erezt.dev <erezt.dev@gmail.com>
2018-04-19 18:51:42 +0300

    Simple getter component created.

---
+++ resources/assets/js/components/GetComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <table v-if="coins">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Year</th>
+        </tr>
+        <tr v-for="coin in coins">
+            <td>{{coin.id}}</td>
+            <td>{{coin.name}}</td>
+            <td>{{coin.price}}</td>
+            <td>{{coin.year}}</td>
+        </tr>
+    </table>
+    
+</template>
+
+<script>
+    export default {
+        //name: "GetComponent"
+        data() {
+            return {
+                coins:{}
+            }
+        },
+        methods: {
+            getCoins() {
+                let uri = 'list';
+                this.axios.get(uri)
+                    .then((response) =>{
+                  this.coins = response.data;
+                })
+            }
+        },
+        created(){
+            this.getCoins();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
